0afdf825c7026353035dafc8b0690c6d6bd0ccb0
Cem <cemkgrbz@gmail.com>
2022-10-10 16:46:01 +0200

    added solution

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import React from "react"; //Main React.js library
 import ReactDOM from "react-dom/client"; //we use ReactDOM to render into the DOM
 
 // only update the value of this array
-const navlinkItems = [];
+const navlinkItems = [
+    <li className="nav-item">
+      <a className="nav-link" href="#">
+        Link to google.com
+      </a>
+    </li>,
+    <li className="nav-item">
+      <a className="nav-link" href="#">
+        Link to facebook.com
+      </a>
+    </li>,
+    <li className="nav-item">
+      <a className="nav-link" href="#">
+        Link to amazon.com
+      </a>
+    </li>,
+  ];
 
-const content = <ul className="nav">{navlinkItems}</ul>;
+  const content = <ul className="nav">{navlinkItems}</ul>;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
